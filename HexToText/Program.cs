ac821cdaebf9dd683e9d4b105882156dbb3728d9
Lucas Brennan <lucasjacbrennan@gmail.com>
2023-04-26 19:16:42 +0100

    Setup Logic for Part 1

--- HexToText/Program.cs
+++ HexToText/Program.cs
@@ -9,14 +9,27 @@ namespace HexToText
             List<string> hexList = new List<string> { "A4", "E6", "16", "76", "56", "C7", "32", "05", "54", "B2", "04", "C7",
                 "46", "42", "E2", "E2", "E2", "E2", "E2", "E2", "E3", "9C", "15", "AF", "43" };
 
-            foreach(string hex in hexList)
+            string textString = string.Empty;
+
+            for (int i = 0; i < 20; i++) 
             {
-                int decimalValue = Convert.ToInt32(hex, 16);
-
-                Console.WriteLine($"The Decimal Value is {decimalValue} and the Char Value is {(char)decimalValue}");
+                textString += (char)Convert.ToInt32(hexList[i], 16);
             }
-            
 
+            DateTime date = new DateTime(1000, 1, 1);
+
+            for(int i = 0; i < 10; i++)
+            {
+                date = date.AddDays(Convert.ToInt32(hexList[i], 16));
+            }
+
+            Console.WriteLine("BitA = " + (Convert.ToInt32(hexList[0].Substring(0 , 1), 16) > 9));
+            Console.WriteLine("BitB = " + (Convert.ToInt32(hexList[1].Substring(0, 1), 16) > 9));
+            Console.WriteLine("BitC = " + (Convert.ToInt32(hexList[2].Substring(0, 1), 16) > 9));
+            Console.WriteLine("BitD = " + (Convert.ToInt32(hexList[3].Substring(0, 1), 16) > 9));
+            Console.WriteLine("TextA = " + textString);
+            Console.WriteLine("ShortA = " + Convert.ToInt16(hexList[0], 16));
+            Console.WriteLine("DateTimeA = " + date.ToString());
         }
     }
 }
